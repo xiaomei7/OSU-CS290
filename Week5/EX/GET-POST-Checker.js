2f636e1e407f240e0df0530f6eac45eadce83806
xiaomei7 <xiaomei.wang07@gmail.com>
2016-07-24 17:52:58 -0700

    HW

--- Week5/EX/GET-POST-Checker.js
+++ Week5/EX/GET-POST-Checker.js
@@ -46,5 +46,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://http://52.35.2.29:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
