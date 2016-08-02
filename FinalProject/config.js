1e6404ce77bec4c6e9fef99082f757afd17b07a3
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 18:53:24 -0700

    O

--- FinalProject/config.js
+++ FinalProject/config.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host  : 'localhost',
+  connectionLimit : 10,
+  host  : '52.35.2.29',
   user  : 'student',
   password: 'default',
   database: 'student'
