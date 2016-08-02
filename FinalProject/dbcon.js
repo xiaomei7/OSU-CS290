34da885dd10d0b4c55c8a3064812920b7bb97beb
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 17:48:54 -0700

    Final

--- FinalProject/dbcon.js
+++ FinalProject/dbcon.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host  : 'localhost',
+  host  : '52.35.2.29',
   user  : 'student',
   password: 'default',
   database: 'student'
--- FinalProject/workoutTracker.js
+++ FinalProject/workoutTracker.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
+var mysql = require('dbcon.js');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -15,7 +15,7 @@ app.get('/', function(req,res){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
@@ -28,4 +28,19 @@ app.get('/reset-table',function(req,res,next){
       res.render('home',context);
     })
   });
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://52.35.2.29:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
