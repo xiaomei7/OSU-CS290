433693bacdfe2e0a65325e28719d385f044f1d3a
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 21:38:50 -0700

    X

--- project/info.js
+++
@@ -1,9 +0,0 @@
-
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'student',
-  password: 'default',
-  database: 'student'
-});
-module.exports.pool = pool;
