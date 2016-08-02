a3175048b3a9cad156744ec1aed0bbe027f201d9
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 18:17:05 -0700

    O

--- Week6/ExpressMysql/dbcon.js
+++ Week6/ExpressMysql/dbcon.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : '52.35.2.29',
-  user            : 'hellouser',
-  password        : 'default',
-  database        : 'hellodb'
+  host  : '52.35.2.29',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
 });
 
 module.exports.pool = pool;
