260878af1f092b7d4d692337b2cc32e1cf0c16eb
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 17:54:08 -0700

    O

--- FinalProject/workoutTracker.js
+++ FinalProject/workoutTracker.js
@@ -9,7 +9,15 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/', function(req,res){
-    res.render('home');
+  var context = {};
+  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+  if(err){
+    next(err);
+    return;
+  }
+  context.results = JSON.stringify(rows);
+  res.render('home', context);
+  });
 });
 
 
