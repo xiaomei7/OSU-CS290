28b684bd6df3cb3a95fb71d05b9bdee39d08731d
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-01 18:41:17 -0700

    O

--- FinalProject/config.js
+++ FinalProject/config.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
+  host            : 'localhost',
   user            : 'cs290_wangx2',
   password        : '9846',
   database        : 'cs290_wangx2'
