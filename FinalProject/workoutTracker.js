7652f74a68c5b5cb381390f8521e428d4bdf139c
xiaomei7 <xiaomei.wang07@gmail.com>
2016-08-02 11:32:57 -0700

    O

--- FinalProject/workoutTracker.js
+++ FinalProject/workoutTracker.js
@@ -3,6 +3,11 @@ var pool = require('./config').pool;
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret:'SuperSecretPassword'}));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -10,80 +15,6 @@ app.set('port', 3000);
 
 app.get('/', function(req,res){
     res.render('home');
-});
-
-
-app.post('/delete', function(req,res) {
-    pool.query('DELETE FROM workouts WHERE id = (?)', [req.body.id], function(err, result){
-        if(err) {
-            next(err);
-            return;
-        }
-    pool.query('SELECT id,name,reps,weight,DATE_FORMAT(date, "%m-%d-%Y") AS date,lbs FROM workouts', function(err, rows, fields){
-            if(err){
-                next(err);
-                return;
-            }
-            res.type('text/plain');
-            res.send(rows);
-        });
-    });
-});
-
-app.get('/edit', function(req,res,next) {
-    pool.query('SELECT id,name,reps,weight,DATE_FORMAT(date, "%Y-%m-%d") AS date,lbs FROM workouts WHERE id=(?)', 
-        [req.query.id], function(err, rows, fields){
-            if(err) {
-                next(err);
-                return;
-            }
-        context = rows[0]; 
-        res.render('edit', context);
-        });
-});
-
-app.get('/update', function(req,res,next) {
-    var context = {};
-    pool.query('UPDATE workouts SET date=?, name=?, reps=?, weight=?, lbs=? WHERE id=?', 
-        [req.query.date, req.query.name, req.query.reps, req.query.weight, req.query.units, req.query.id], 
-        function(err, result){
-            if(err) {
-                next(err);
-                return;
-            }
-        res.render('home');
-        });
-});
-
-
-app.get('/getall',function(req,res,next){
-    pool.query('SELECT id,name,reps,weight,DATE_FORMAT(date, "%m-%d-%Y") AS date,lbs FROM workouts', function(err, rows, fields){
-        if(err){
-            next(err);
-            return;
-        }
-        res.type('text/plain');
-        res.send(rows);
-    });
-});
-
-app.post('/insert', function(req,res,next){
-    pool.query("INSERT INTO workouts (date, name, reps, weight, lbs) VALUES (?,?,?,?,?)", 
-        [req.body.date, req.body.name, req.body.reps, req.body.weight, req.body.units], 
-        function(err, result) {
-        if(err) {
-            next(err);
-            return;
-        }
-        pool.query('SELECT id,name,reps,weight,DATE_FORMAT(date, "%m-%d-%Y") AS date,lbs FROM workouts', function(err, rows, fields){
-            if(err){
-                next(err);
-                return;
-            }
-            res.type('text/plain');
-            res.send(rows);
-        });
-    });
 });
 
 
